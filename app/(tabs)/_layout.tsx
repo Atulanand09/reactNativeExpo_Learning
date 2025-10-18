1dba797203e4ce0cce794ce16227077f61fc42c9
Atulanand09 <atulanand092004@gmail.com>
2025-10-18 19:26:26 +0530

    setup,stack,tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,49 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+const TabsLayout = () => {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+        headerStyle: { backgroundColor: "orange" },
+        headerTitleStyle: { color: "black", fontWeight: "bold" },
+        tabBarActiveTintColor: "orange",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="about"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "About",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="information-circle-outline"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
         }}
       />
     </Tabs>
   );
-}
+};
+
+export default TabsLayout;
---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View style={{ backgroundColor: "red",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+     }}>
+      <Text style={{fontSize:20,fontWeight:"700",color:"white"}}>Hello , Wllcome to My-First App By Atul Anand 
+      </Text>
+      <Link href="/about" style={{marginTop:20,fontSize:16,color:"yellow"}}>Go to About Page</Link>
+    </View>
+  );
+};
+
+export default Index;
